abe06269415221ad9f2b28b011d2c93a29dbb2f7
Benjamin SOYER <benjamin.soyer27@gmail.com>
2024-01-24 19:50:24 +0100

    components creation : pills, card, button

---
+++ src/lib/components/CardListeux.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<div class="card">
+    cacacacacaca je fais des test tu connais
+</div>
+
+<style lang=scss>
+    .card{
+        padding: 1rem;
+        padding-top: 0.3rem;
+        padding-bottom: 0.3rem;
+        border-radius: 0.3rem;
+        background: var(--bg);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
+<script lang="ts">
+    import Button from '$lib/components/Button.svelte';
+	import CardListeux from '$lib/components/CardListeux.svelte';
+
+</script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<section>
+    <p>deziz</p>
+    <Button>petit caca</Button>
+    <CardListeux></CardListeux>
+</section>
+
+
+<style lang=scss>
+    @use '../desing/app.scss' as *;
+    section{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        background: var(--cardBg);
+    }
+</style>
