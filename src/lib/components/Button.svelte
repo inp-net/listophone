199b365f883a8862a060ca24a2641a9bafa213c2
Etienne <etienne.bardet@gmail.com>
2024-01-26 18:17:33 +0100

    Revert "feat: better button"
    This reverts commit 0a1b12dd6224a180d5ad099b703b79b5e7cea9af.

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -5,15 +5,3 @@
 <button>
     <slot />
 </button>
-
-<style>
-    button{
-        padding: 0.4rem;
-        font-size: 1.25em;
-        border-radius: 0.3rem;
-        background: var(--cardBg);
-        color: var(--text);
-        border: 0;
-        width:130px;
-    }
-</style>
